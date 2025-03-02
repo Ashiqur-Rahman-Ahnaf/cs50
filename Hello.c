7efba76ff9ca62d12b4902f857ad38e92636a0ff
Ashiqur Rahman Ahnaf <132572748+Ashiqur-Rahman-Ahnaf@users.noreply.github.com>
2025-03-02 10:05:04 +0000

    Add initial C file and other updates

--- Hello.c
+++ Hello.c
@@ -1,9 +1,9 @@
 #include <cs50.h>
-#include <stdio.h>
+echo ' #include <stdio.h>
 
 int main(void)
 {
     string name = get_string("What is your name? ");
     printf("Hello, %s!\n", name);
     return 0;
-}
+}'
